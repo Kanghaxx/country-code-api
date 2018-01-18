6706f27d3e8065d34b74118b9c56f00231e6c6f6
AlexPodshivalov <epicdevelop@gmail.com>
2018-01-18 03:16:24 +0300

    Create unit tests for Country API
    Relates to #2
    Add unit tests of Get and Find methods of Country controller

--- Testing.UnitTests.Data.Model/Controller/CountryControllerTests.cs
+++ Testing.UnitTests.Data.Model/Controller/CountryControllerTests.cs
@@ -8,10 +8,12 @@ using System.Threading.Tasks;
 using Data.Common.Model;
 using Data.Common.Abstract;
 using Web.API.Controllers;
+using System.Web.Http.Results;
+using Web.API.Models;
 
 namespace Testing.UnitTests.Data.Model.Controller
 {
-    [TestCategory("Web.Controller.CountryController")]
+    [TestCategory("Web.Controllers.CountryController")]
     [TestClass]
     public class CountryControllerTests
     {
@@ -37,18 +39,172 @@ namespace Testing.UnitTests.Data.Model.Controller
 
             var repMock = new Mock<ICountryRepository>();
             repMock.Setup(m => m.GetAsync()).Returns(asyncData);
-
             var uowMock = new Mock<IUnitOfWork>();
             uowMock.Setup(m => m.CountryRepository).Returns(repMock.Object);
-
             var factoryMock = new Mock<IStoreFactory>();
             factoryMock.Setup(m => m.CreateUnitOfWork()).Returns(uowMock.Object);
 
             var c = new CountryController(factoryMock.Object);
 
             var result = await c.GetCountries();
+            var contentResult = result as OkNegotiatedContentResult<IEnumerable<CountryDTO>>;
 
-            Assert.IsNotNull(result);
+            Assert.IsNotNull(contentResult);
+            Assert.IsTrue(contentResult.Content.Count() == 3);
+        }
+
+        [TestMethod]
+        public async Task GetCountry_Returns_FoundItem()
+        {
+            var c1 = new Country()
+            {
+                CountryId = 2,
+                IsoCode = "BB",
+                Name = "C2"
+            };
+            var data = new List<Country>()
+            {
+                new Country()
+                {
+                    CountryId = 1, IsoCode = "AA", Name = "C1"
+                },
+                c1
+                ,
+                new Country()
+                {
+                    CountryId = 3, IsoCode = "CC", Name = "C3"
+                },
+            };
+
+            var repMock = new Mock<ICountryRepository>();
+            repMock.Setup(m => m.GetAsync(It.IsAny<string>())).Returns(Task.FromResult(c1));
+            var uowMock = new Mock<IUnitOfWork>();
+            uowMock.Setup(m => m.CountryRepository).Returns(repMock.Object);
+            var factoryMock = new Mock<IStoreFactory>();
+            factoryMock.Setup(m => m.CreateUnitOfWork()).Returns(uowMock.Object);
+
+            var c = new CountryController(factoryMock.Object);
+
+            var result = await c.GetCountry("BB");
+            var contentResult = result as OkNegotiatedContentResult<CountryDTO>;
+
+            Assert.IsNotNull(contentResult);
+            Assert.IsTrue(contentResult.Content.IsoCode == c1.IsoCode);
+            Assert.IsTrue(contentResult.Content.Name == c1.Name);
+        }
+
+        [TestMethod]
+        public async Task GetCountry_Returns_404_IfNotFound()
+        {
+            var c1 = new Country()
+            {
+                CountryId = 2,
+                IsoCode = "BB",
+                Name = "C2"
+            };
+            var data = new List<Country>()
+            {
+                new Country()
+                {
+                    CountryId = 1, IsoCode = "AA", Name = "C1"
+                },
+                c1
+                ,
+                new Country()
+                {
+                    CountryId = 3, IsoCode = "CC", Name = "C3"
+                },
+            };
+
+            var repMock = new Mock<ICountryRepository>();
+            repMock.Setup(m => m.GetAsync("BB")).Returns(Task.FromResult<Country>(null));
+            var uowMock = new Mock<IUnitOfWork>();
+            uowMock.Setup(m => m.CountryRepository).Returns(repMock.Object);
+            var factoryMock = new Mock<IStoreFactory>();
+            factoryMock.Setup(m => m.CreateUnitOfWork()).Returns(uowMock.Object);
+
+            var c = new CountryController(factoryMock.Object);
+
+            var result = await c.GetCountry("BB");
+
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        }
+
+
+        [TestMethod]
+        public async Task Find_Returns_FoundItems()
+        {
+            var data = new List<Country>()
+            {
+                new Country()
+                {
+                    CountryId = 2,
+                    IsoCode = "BB",
+                    Name = "C2"
+                },
+                new Country()
+                {
+                    CountryId = 3, IsoCode = "CC", Name = "C3"
+                },
+            };
+
+            var repMock = new Mock<ICountryRepository>();
+            repMock.Setup(m => m.FindAsync(new string[] { "BB", "CC"}))
+                .Returns(Task.FromResult(data.AsEnumerable()));
+            var uowMock = new Mock<IUnitOfWork>();
+            uowMock.Setup(m => m.CountryRepository).Returns(repMock.Object);
+            var factoryMock = new Mock<IStoreFactory>();
+            factoryMock.Setup(m => m.CreateUnitOfWork()).Returns(uowMock.Object);
+
+            var c = new CountryController(factoryMock.Object);
+
+            var result = await c.FindCountries(new SearchDTO()
+            {
+                IsoCodes = new string[] { "BB", "CC" }
+            });
+            var contentResult = result as OkNegotiatedContentResult<IEnumerable<CountryDTO>>;
+
+            Assert.IsNotNull(contentResult);
+            Assert.IsTrue(contentResult.Content.Count() == 2);
+            Assert.IsNotNull(contentResult.Content.Where(x => x.IsoCode == "BB"));
+            Assert.IsNotNull(contentResult.Content.Where(x => x.IsoCode == "CC"));
+        }
+
+
+        [TestMethod]
+        public async Task Find_Returns_404_IfNotFound()
+        {
+            var data = new List<Country>()
+            {
+                new Country()
+                {
+                    CountryId = 2,
+                    IsoCode = "BB",
+                    Name = "C2"
+                },
+                new Country()
+                {
+                    CountryId = 3, IsoCode = "CC", Name = "C3"
+                },
+            };
+
+            var repMock = new Mock<ICountryRepository>();
+            repMock.Setup(m => m.FindAsync(new string[] { "BB", "CC" }))
+                .Returns(Task.FromResult<IEnumerable<Country>>(null));
+            var uowMock = new Mock<IUnitOfWork>();
+            uowMock.Setup(m => m.CountryRepository).Returns(repMock.Object);
+            var factoryMock = new Mock<IStoreFactory>();
+            factoryMock.Setup(m => m.CreateUnitOfWork()).Returns(uowMock.Object);
+
+            var c = new CountryController(factoryMock.Object);
+
+            var result = await c.FindCountries(new SearchDTO()
+            {
+                IsoCodes = new string[] { "BB", "CC" }
+            });
+
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+            
         }
     }
 }
