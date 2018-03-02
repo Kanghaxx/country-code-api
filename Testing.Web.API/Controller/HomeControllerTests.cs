d39dc9f27158fef5e6787675581653102b7b0e90
AlexPodshivalov <epicdevelop@gmail.com>
2018-03-02 21:50:00 +0300

    Fix the Get_Returns_Endpoints unit test
    Fix #39

--- Testing.Web.API/Controller/HomeControllerTests.cs
+++ Testing.Web.API/Controller/HomeControllerTests.cs
@@ -25,6 +25,7 @@ namespace Testing.Web.API.Controller
             urlHelper.Setup(m => m.Link("GetCountries", null)).Returns("api/GetCountries");
             urlHelper.Setup(m => m.Link("GetCurrencies", null)).Returns("api/GetCurrencies");
             urlHelper.Setup(m => m.Link("GetOrganizations", null)).Returns("api/GetOrganizations");
+            urlHelper.Setup(m => m.Content("~/api/login")).Returns("/api/login");
             urlHelper.Setup(m => m.Content("~/api/help/index")).Returns("api/help/index");
 
             var c = new HomeController();
@@ -35,10 +36,11 @@ namespace Testing.Web.API.Controller
             var contentResult = result as OkNegotiatedContentResult<GetEndpointsResult>;
 
             Assert.IsNotNull(contentResult);
-            Assert.IsTrue(contentResult.Content.Endpoints.Count() == 4);
+            Assert.IsTrue(contentResult.Content.Endpoints.Count() == 5);
             Assert.IsTrue(contentResult.Content.Endpoints["GetCountries"] == "api/GetCountries");
             Assert.IsTrue(contentResult.Content.Endpoints["GetCurrencies"] == "api/GetCurrencies");
             Assert.IsTrue(contentResult.Content.Endpoints["GetOrganizations"] == "api/GetOrganizations");
+            Assert.IsTrue(contentResult.Content.Endpoints["GetOAuthToken"] == "/api/login");
             Assert.IsTrue(contentResult.Content.Endpoints["GetHelpHtml"] == "api/help/index");
         }
         
